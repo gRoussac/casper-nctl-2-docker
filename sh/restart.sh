fa8475802c6c2b9cfcfc5289b3f9dd3c02dcd750
gRoussac <gRoussac@users.noreply.github.com>
2024-11-07 04:36:05 +0100

    small update\n

--- sh/restart.sh
+++ sh/restart.sh
@@ -11,6 +11,7 @@ else
 fi
 
 sed -i 's/^allow_request_speculative_exec = false/allow_request_speculative_exec = true/' ./casper-node/resources/local/config.toml
+# sed -i 's/^enable_addressable_entity = false/enable_addressable_entity = true/' ./casper-node/resources/local/chainspec.toml.in
 nctl-assets-setup
 find $NCTL/assets/ -name 'secret_key.pem' -exec chmod go+r {} \;
 nctl-start
