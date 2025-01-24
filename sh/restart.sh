36376b5c9eb3b6472235bab68ae8d9e371c4d7a3
gRoussac <gRoussac@users.noreply.github.com>
2025-01-24 23:42:44 +0100

    Update branches\n

--- sh/restart.sh
+++ sh/restart.sh
@@ -10,8 +10,10 @@ else
     source /app/casper-nctl/activate
 fi
 
-sed -i 's/^allow_request_speculative_exec = false/allow_request_speculative_exec = true/' ./casper-node/resources/local/config.toml
+# sed -i 's/^allow_request_speculative_exec = false/allow_request_speculative_exec = true/' ./casper-node/resources/local/config.toml
 # sed -i 's/^enable_addressable_entity = false/enable_addressable_entity = true/' ./casper-node/resources/local/chainspec.toml.in
+# sed -i 's/^block_gas_limit = 200_000_000_000/block_gas_limit = 600_000_000_000/' ./casper-node/resources/local/chainspec.toml.in
+# sed -i 's/^qps_limit = 110/qps_limit = 220/' ./casper-node/resources/local/config.toml
 nctl-assets-setup
 find $NCTL/assets/ -name 'secret_key.pem' -exec chmod go+r {} \;
 nctl-start
