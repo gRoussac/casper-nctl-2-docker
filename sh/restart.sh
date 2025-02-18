2c8692a9c92d67e9420a0ef59c7fbe1099579e44
gRoussac <gRoussac@users.noreply.github.com>
2025-02-18 15:00:37 +0100

    Update config\n

--- sh/restart.sh
+++ sh/restart.sh
@@ -2,18 +2,19 @@
 set -m
 
 # Check if an argument is provided
-if [ -n "$1" ]; then
-    # Use the provided path as the activation script
-    source $1/activate
-else
-    # Use the default path as the activation script
+if [ -n "$1" ] && [ -f "$1/activate" ]; then
+    source "$1/activate"
+elif [ -f "/app/casper-nctl/activate" ]; then
     source /app/casper-nctl/activate
+else
+    echo "Error: Activation script not found"
+    exit 1
 fi
-
-# sed -i 's/^allow_request_speculative_exec = false/allow_request_speculative_exec = true/' ./casper-node/resources/local/config.toml
+sed -i 's/^allow_request_speculative_exec = false/allow_request_speculative_exec = true/' ./casper-node/resources/local/config.toml
 # sed -i 's/^enable_addressable_entity = false/enable_addressable_entity = true/' ./casper-node/resources/local/chainspec.toml.in
+sed -i 's/262_144/450_000/g' ./casper-node/resources/local/chainspec.toml.in
 # sed -i 's/^block_gas_limit = 200_000_000_000/block_gas_limit = 600_000_000_000/' ./casper-node/resources/local/chainspec.toml.in
-# sed -i 's/^qps_limit = 110/qps_limit = 220/' ./casper-node/resources/local/config.toml
+sed -i 's/^qps_limit = 110/qps_limit = 220/' ./casper-node/resources/local/config.toml
 nctl-assets-setup
 find $NCTL/assets/ -name 'secret_key.pem' -exec chmod go+r {} \;
 nctl-start
