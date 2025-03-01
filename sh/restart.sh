9adcf18c1527132c87e6345a1d4a02783af420eb
gRoussac <gRoussac@users.noreply.github.com>
2025-03-01 01:49:46 +0100

    qps_limit\n

--- sh/restart.sh
+++ sh/restart.sh
@@ -15,6 +15,7 @@ sed -i 's/^allow_request_speculative_exec = false/allow_request_speculative_exec
 sed -i 's/262_144/450_000/g' ./casper-node/resources/local/chainspec.toml.in
 # sed -i 's/^block_gas_limit = 200_000_000_000/block_gas_limit = 600_000_000_000/' ./casper-node/resources/local/chainspec.toml.in
 sed -i 's/^qps_limit = 110/qps_limit = 220/' ./casper-node/resources/local/config.toml
+sed -i 's/^qps_limit = 1$/qps_limit = 100/' ./casper-sidecar/resources/example_configs/default_rpc_only_config.toml
 nctl-assets-setup
 find $NCTL/assets/ -name 'secret_key.pem' -exec chmod go+r {} \;
 nctl-start
